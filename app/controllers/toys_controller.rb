5d1bfa0caddfd4779ffe57ddea9e75d4b5487cec
khristine-g <githigekhristine@gmail.com>
2023-06-27 10:01:43 +0300

    completed assignment

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,31 +1,49 @@
-class ToysController < ApplicationController
-  wrap_parameters format: []
+def index
+  toys = Toy.all
+  render json: toys
+  render json: toys, except: [:created_at, :updated_at]
+end
 
-  def index
-    toys = Toy.all
-    render json: toys
-  end
+def create
+  toy = Toys.create(toy_params)
+  render json: toy, status: :created
+  render json: toy, except: [:created_at, :updated_at], status: :created
+end
 
-  def create
-    toy = Toys.create(toy_params)
-    render json: toy, status: :created
-  end
+def update
+  toy = Toy.find_by(id: params[:id])
+  # toy = Toy.find_by(id: params[:id])
+  # toy.update(toy_params)
+  toy = find_toy
+  toy.update(toy_params)
+  render json: toy, except: [:created_at, :updated_at], status: :ok
+rescue ApplicationRecord::RecordNotFound
+  render_not_found_response
+end
 
-  def update
-    toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
-  end
+def destroy
+  toy = Toy.find_by(id: params[:id])
+  # toy = Toy.find_by(id: params[:id])
+  # toy.destroy
+  # head :no_content
+  toy = find_toy
+  toy.destroy
+  head :no_content
+rescue ApplicationRecord::RecordNotFound
+  render_not_found_response
+end
 
-  def destroy
-    toy = Toy.find_by(id: params[:id])
-    toy.destroy
-    head :no_content
-  end
+private
+def toy_params
+  params.permit(:name, :image, :likes)
+end
 
-  private
-  
-  def toy_params
-    params.permit(:name, :image, :likes)
-  end
+def find_toy
+  Toy.find(params[:id])
+end
+
+def render_not_found_response
+  render json: { error: "toy not found" }, status: :not_found
+end
 
 end
